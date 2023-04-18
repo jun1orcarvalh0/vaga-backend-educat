06843b2ccb64508bcfbaf9add21b2d736fc8ba04
Junior <antoniomarcosdecj18@gmail.com>
2023-04-18 12:50:49 -0300

    [refactor] Adicionando o campo nome na classe InstituicaoX e adicionando o metodo str nas classes

--- back-end/instituicao_x/models.py
+++ back-end/instituicao_x/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 
+
 class Aluno(models.Model):
     nome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=11)
     email = models.CharField(max_length=100, null=True, blank=True)
-    
+
+    def __str__(self):
+        return f'{self.nome} - {self.cpf}'
+
 
 class Disciplina(models.Model):
     nome = models.CharField(max_length=20)
-    
+
+    def __str__(self):
+        return f'{self.nome}'
+
 
 class Status(models.Model):
     nome = models.CharField(max_length=20)
-    
+
+    def __str__(self):
+        return f'{self.nome}'
+
 
 class InstituicaoX(models.Model):
+    nome = models.CharField(max_length=200)
     aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
     ano_letivo = models.IntegerField()
     semetre = models.IntegerField()
@@ -22,3 +33,9 @@ class InstituicaoX(models.Model):
     carga_horaria = models.IntegerField()
     frequencia = models.IntegerField()
     status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = 'Instituição'
+
+    def __str__(self):
+        return f'{self.nome}'
