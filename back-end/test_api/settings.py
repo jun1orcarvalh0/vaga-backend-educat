d8c8890543b6497939d9c73fcb3209eebe28c43e
Junior <antoniomarcosdecj18@gmail.com>
2023-04-18 13:05:10 -0300

    Adicionando a instituicao_x no installed apps e criando as migrations

--- back-end/test_api/settings.py
+++ back-end/test_api/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'instituicao_y.apps.InstituicaoYConfig',
-    # Escreva o necessario abaixo
+    'instituicao_x.apps.InstituicaoXConfig'
 ]
 
 MIDDLEWARE = [
