f532b7481e4e72baae8927fa23a3d6cc8c29aa33
Junior <antoniomarcosdecj18@gmail.com>
2023-04-18 19:59:27 -0300

    [feat] Através da AlunoView criei uma rota que espera um parametro de query 'cpf' e retorna o usuário caso ele exista no banco

--- back-end/instituicao_y/views.py
+++ back-end/instituicao_y/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 
 from .models import Aluno, Status, Disciplina, InstituicaoY
 from .serializers import AlunoSerializer, StatusSerializer, DisciplinaSerializer, InstituicaoYSerializer
@@ -10,6 +13,26 @@ class AlunoView(viewsets.ModelViewSet):
     serializer_class = AlunoSerializer
 
 # Escreva o necessário abaixo
+
+    @action(detail=False)
+    def recuperar_aluno(self, request, pk=None):
+        cpf = request.query_params.get('cpf')
+        if not cpf:
+            return Response(
+                {"details": "O campo CPF não foi enviado"},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        find_user = Aluno.objects.filter(cpf=cpf)
+
+        if not find_user:
+            return Response(
+                {"details": "O Estudante não foi encontrado"},
+                status=status.HTTP_404_NOT_FOUND
+                )
+
+        serializer = self.get_serializer(find_user, many=True)
+
+        return Response(serializer.data)
 
 
 class StatusView(viewsets.ModelViewSet):
