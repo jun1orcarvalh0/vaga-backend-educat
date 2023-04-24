25cbcce227f5f3a0c5bfac5088d716ba4daaf98d
Junior <antoniomarcosdecj18@gmail.com>
2023-04-24 17:55:26 -0300

    [feat] Adicionando o CORS no back-end para que requisicoes do front-end sejam permitidas

--- back-end/test_api/settings.py
+++ back-end/test_api/settings.py
@@ -40,17 +40,23 @@ INSTALLED_APPS = [
     'rest_framework',
     'instituicao_y.apps.InstituicaoYConfig',
     # escreva o que for necessário abaixo
-    'instituicao_x.apps.InstituicaoXConfig'
+    'instituicao_x.apps.InstituicaoXConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'test_api.urls'
